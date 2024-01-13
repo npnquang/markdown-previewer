499006959049fca06e93080fc27d8640850ce696
Pham Nhat Quang Nguyen <nguyenjoe0201@gmail.com>
2024-01-13 11:27:47 +0700

    Add the DOMPurify to prevent dirty HTML string

--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import parse from "html-react-parser";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import DOMPurify from "dompurify";
 
 export default function Previewer({ text }) {
+  const parsedHTML = DOMPurify.sanitize(marked.parse(text));
+  const  display = parse(parsedHTML);
   
-  
+
   return (
     <div id="preview">
-      {parse(marked.parse(text))}
+      {display}
     </div>
   )
 }
