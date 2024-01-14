572b6a20326c3bf95397f7b7bb4d73ebd9afa93c
Pham Nhat Quang Nguyen <nguyenjoe0201@gmail.com>
2024-01-14 21:27:13 +0700

    Add the feature to accept the Tab in the editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,8 +6,21 @@ export default function Editor({setText, initialText}) {
         setText(event.target.value);
     }
 
+    const handleKeyDown = (event) => {
+        if (event.code === "Tab") {
+            event.preventDefault();
+            const start = event.target.selectionStart;
+            const end = event.target.selectionEnd;
+                event.target.value = event.target.value.substring(0, start)
+                + "    "
+                + event.target.value.substring(end);
+            event.target.selectionStart = end + 4;
+            event.target.selectionEnd = end + 4;
+        }
+    }
+
     return (
-        <textarea id="editor" label="editor" onChange={handleChange} defaultValue={initialText}></textarea>
+        <textarea id="editor" label="editor" onChange={handleChange} onKeyDown={handleKeyDown} defaultValue={initialText}></textarea>
     )
 }
 
